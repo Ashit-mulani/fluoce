d45588521ac301e8af8c6e9be2eced5e257e36ff
Ashit-mulani <mulaniashit@gmail.com>
2025-12-23 17:25:32 +0530

    Update FormHeader component to change title text and adjust class order for consistency

--- clients/auth/src/components/ui/FormHeader.jsx
+++ clients/auth/src/components/ui/FormHeader.jsx
@@ -3,9 +3,9 @@ import { Logo } from "./Logo";
 
 const FormHeader = () => {
   return (
-    <div className="flex gap-2 items-center">
+    <div className="flex items-center gap-2">
       <Logo thickness={2} size={28} className="shrink-0" />
-      <h1 className="font-medium SSN text-xl">Welcome to Fluoce</h1>
+      <h1 className="SSN text-xl font-medium">Welcome to Fluoce Cloud</h1>
     </div>
   );
 };
